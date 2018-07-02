cc5d30afc6ad6b0583df2db2d919967f8323a7cb
tgaram <tgaram@gmail.com>
2018-07-02 18:38:31 +0200

    send data in online event instead sync

--- js/common.js
+++ js/common.js
@@ -4,14 +4,7 @@
         console.log('Registration worked!');        
       }).catch(function(err) {
         console.log('Registration failed!', err);
-      });
-
-      navigator.serviceWorker.ready.then(function(swRegistration) {
-        console.log('sw registration sync ready');
-        swRegistration.sync.register('submitPendingFavorites');
-        swRegistration.sync.register('submitPendingReviews');
-      });
-           
+      });     
     });
   }
 
@@ -59,15 +52,49 @@ window.setTimeout(function() {
   
 }, 2000);
 
-window.onload = function() { 
-  console.log(document.getElementById('offline-alert'));
-  window.addEventListener('offline', function(e) { 
-    console.log('offline'); 
-    document.getElementById('offline-alert').style.display = 'block';
-  });
 
-  window.addEventListener('online', function(e) { 
-    console.log('online');
-    document.getElementById('offline-alert').style.display = 'none'; 
+function checkPendingReviews() {
+  let dbPromise = DBHelper.openIdbDatabase();
+  dbPromise.then(function(db) {
+    if(!db) return;
+    var tx = db.transaction('pending-reviews', 'readwrite');
+    var store = tx.objectStore('pending-reviews');
+    return store.getAll();
+  }).then(function(pendingReviews) {
+    pendingReviews.forEach(function(review) {
+      DBHelper.deletePendingReviewFromIdb(review);
+      DBHelper.storeReview(review, function (review) {
+        console.log('submiting pending reviews');
+      });   
+    });
   });
 }
+
+
+function checkPendingFavorites() {
+  let dbPromise = DBHelper.openIdbDatabase();
+  dbPromise.then(function(db) {
+    if(!db) return;
+    var tx = db.transaction('restaurants', 'readwrite');
+    var store = tx.objectStore('restaurants');
+    return store.getAll();
+  }).then(function(restaurants) {
+    restaurants.forEach(function(restaurant) {
+      if (restaurant.is_favorite_pending == "true" || restaurant.is_favorite_pending == true) {
+        DBHelper.submitRestaurantPendingFavorites(restaurant);
+      }
+    });
+  });
+}
+  
+window.addEventListener('offline', function(e) { 
+  console.log('offline'); 
+  document.getElementById('offline-alert').style.display = 'block';
+});
+
+window.addEventListener('online', function(e) { 
+  console.log('online');
+  document.getElementById('offline-alert').style.display = 'none';
+  checkPendingReviews();
+  checkPendingFavorites();
+});
